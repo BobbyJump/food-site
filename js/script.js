236abb8d14ed2823d7fa56c03f9de98e9dcf2c0a
Dmitriy <skachok.d@gmail.com>
2022-07-01 23:43:37 +0300

    add modal functionality

--- js/script.js
+++ js/script.js
@@ -98,4 +98,38 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     
     setTimer('.timer', futureDate);
+
+    // Modal
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+    
+
+    const closeModal = () => {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    };     
+    
+    const openModal = () => {
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerID);
+    };
+
+    const modalTimerID = setTimeout(openModal, 15000);
+
+    modalTrigger.forEach(item => {item.addEventListener('click', openModal);});
+    modalCloseBtn.addEventListener('click', closeModal);
+    modal.addEventListener('click', (e) => {if(e.target === modal) {closeModal();}});
+
+    const openModalByScroll = () => {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            clearInterval(modalTimerID);
+            window.removeEventListener('scroll', openModalByScroll);
+        }
+    };
+
+    window.addEventListener('scroll', openModalByScroll);
+
 });
