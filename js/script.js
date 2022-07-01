10a136ad63898b4f63d57c6f901e1c413067d87d
Dmitriy <skachok.d@gmail.com>
2022-07-01 17:47:16 +0300

    created functionality for the countdown timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Tabs
     const tabs = document.querySelectorAll('.tabheader__item'), 
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -34,4 +35,59 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // Timer
+
+    const futureDate = `2022-07-06`;
+    
+    const getRemainingTime = endDate => {
+        const time = Date.parse(endDate) - Date.parse(new Date()),
+              days = Math.floor(time / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((time / (1000 * 60 * 60)) % 24),
+              minutes = Math.floor((time / 1000 / 60) % 60),
+              seconds = Math.floor((time / 1000) % 60);
+
+        return {
+            'total': time,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    };
+
+    const getZero = (num) => {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    };
+
+    const setTimer = (selector, endTime) => {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateTimer, 1000);
+
+        updateTimer();
+
+        function updateTimer() {
+            const time = getRemainingTime(endTime);
+
+            days.innerHTML = getZero(time.days);
+            hours.innerHTML = getZero(time.hours);
+            minutes.innerHTML = getZero(time.minutes);
+            seconds.innerHTML = getZero(time.seconds);
+
+            if (time.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    };
+    
+    setTimer('.timer', futureDate);
 });
